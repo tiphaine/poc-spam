f5c0a576dcb9bea7d8593c2119f29a80f3d10c7a
Tiphaine Phe-Neau <tiph.pheneau@gmail.com>
2016-10-03 15:17:00 +0200

    Commit simple spam classifier version
    I quickly wrote this to help out a friend.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # poc-spam
+
+Simple python script to train a RandomForestClassifier for spam/ham classification, model saving and querying.
+
+I quickly wrote this to help out a friend who had spam issues on his website. This uses TF-IDF vectorizer only at the moment for features. Improved features will appear next if required.
+
+
+
---
+++ spam.py
@@ -0,0 +1,65 @@
+import click
+
+
+from sklearn.datasets import load_files
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+
+MODEL_SAVE = 'rf-clf.pkl'
+TARGET_SAVE = 'target.pkl'
+VECT_SAVE= 'vectorizer.pkl'
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def format():
+    pass
+
+
+@cli.command()
+@click.option('--data', default='dataset')
+@click.option('--test_size', default=0.1)
+@click.option('--save/--no-save', default=False)
+def train(data, save, test_size):
+    dataset = load_files(data)
+    X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=test_size)
+    vectorizer = TfidfVectorizer(min_df=5,
+                                 max_df = 0.8,
+                                 sublinear_tf=True,
+                                 use_idf=True)
+    train_vectors = vectorizer.fit_transform(X_train)
+    test_vectors = vectorizer.transform(X_test)
+    clf = RandomForestClassifier()
+    clf.fit(train_vectors, y_train)
+    accuracy = accuracy_score(y_test, clf.predict(test_vectors))
+    click.echo('Accuracy: {}'.format(accuracy))  
+    if save:
+        joblib.dump(dataset.target_names, TARGET_SAVE) 
+        joblib.dump(clf, MODEL_SAVE) 
+        joblib.dump(vectorizer, VECT_SAVE)
+        click.echo(
+            'Model saved in \'{}\'.\nVectorizer saved in \'{}\'.\nTarget names saved in \'{}\'.'.format(
+                MODEL_SAVE, VECT_SAVE, TARGET_SAVE))
+
+
+@cli.command()
+@click.argument('msg')
+@click.option('--model', default=MODEL_SAVE)
+@click.option('--vectorizer', default=VECT_SAVE)
+@click.option('--target_names', default=TARGET_SAVE)
+def query(msg, model, vectorizer, target_names):
+    clf = joblib.load(model)
+    vect = joblib.load(vectorizer)
+    targets = joblib.load(target_names)
+    msg_vector = vect.transform([msg])
+    click.echo(targets[clf.predict(msg_vector)[0]])
+
+if __name__ == '__main__':
+    cli()
